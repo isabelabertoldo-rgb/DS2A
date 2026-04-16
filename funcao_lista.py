6c434442acb711564dd6076f2415231f1fbacd57
isa.berts <isabela.bertoldo@aluno.senai.br>
2026-04-16 10:45:08 -0300

    Add files via upload

---
+++ funcao_lista.py
@@ -0,0 +1,7 @@
+def listar_alunos(lista):
+    print("/n ---LISTA DE PRESENÇA ---")
+    for aluno in lista:
+        print(f"Estudantes:{aluno}")
+
+turma_ti = ["Gustavo", "joão", "Samuel"]
+listar_alunos(turma_ti)
